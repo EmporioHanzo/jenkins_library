70fed04b254cb95810651407c64c758b060e8ace
EmporioHanzo <goerlitz.k@gmail.com>
2024-02-21 10:57:32 +0000

    modified:   vars/mavenBuildsPipeLine.groovy

--- vars/mavenBuildsPipeLine.groovy
+++ vars/mavenBuildsPipeLine.groovy
@@ -7,7 +7,7 @@ def call() {
     //body.delegate = config
     //body()
 
-    def props = readProperties  file:libraryResource('config.properties')
+    def props = new Properties(libraryResource('config.properties'))
     def maven = props['maven']
     def cron = props['cron']
     def label = props['label']
